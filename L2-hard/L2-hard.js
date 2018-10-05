36acfd7e489905d300a1aff4977130e6578de5ed
ChenTsu <wox.chentsu@gmail.com>
2018-10-05 15:35:24 +0300

    lesson2 unnecessarily part - strings from arr that started at '3' or '7'

--- L2-hard/L2-hard.js
+++ L2-hard/L2-hard.js
@@ -18,3 +18,11 @@ for (let i=0; i<7; i++){
 
 document.write(content);
 
+
+let arr = ['234765', '782367889', '542327', '3567578', '123434', '75452354', '354518765'];
+
+for (let i=0; i<arr.length; i++){
+  if (arr[i][0] === '3' || arr[i][0] === '7'){
+    console.log(arr[i]);
+  }
+}
